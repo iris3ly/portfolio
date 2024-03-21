c5cf8758458798304bb5b0945da4a053c7296f82
iris3ly <irisely2025@u.northwestern.edu>
2024-03-21 14:18:59 -0500

    added react dom router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-
 import Phaser from 'phaser';
 import { PhaserGame } from './game/PhaserGame';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import About from './pages/about.jsx';
+import Projects from './pages/projects.jsx';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "about",
+        element: <About/>,
+    },
+    {
+        path: "projects",
+        element: <Projects/>,
+    },
+    
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
